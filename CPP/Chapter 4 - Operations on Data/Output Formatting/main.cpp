5df8bcf9c39750d0568619a01527ff5f1cac6c11
Arun Jyoti Mondal <ajmonedu@gmail.com>
2022-09-25 22:16:58 +0600

    Note taken on output formatting

---
+++ CPP/Chapter 4 - Operations on Data/Output Formatting/main.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+int main(){
+
+    //std :: flush          flush the output stream
+
+    // header ---------------<iomanip>
+
+    // std :: setw()        specifies the width value of data
+    // std :: setprecision  shows the digit as the precision value is set. 
+    // std :: setfill()     fill the gap with the selected character
+
+    // header ---------------<ios>
+
+    // std :: right         data will be right justified
+    // std :: left          data will be left justified
+    // std :: internal      data will be interanlly positioned as : sign is left justified : data is right justified
+    // std :: boolalpha     force showing the boolean output as the the actual "true" or "false" value
+    // std :: noboolalpha   opposite of boolalpha
+    // std :: showpos       show or hide the + sign for the positive numbers
+    // std :: noshowpos     does the opposite of showpos
+    // std :: dec           shows data in decimal format
+    // std :: oct           shows data in octal format
+    // std :: hex           shows data in hexadecimal format
+    // std :: showbase      shows the base for integral types
+    // std :: uppercase     show data in uppercase
+    /* std :: scientific[default format]            it can be used to force show the data in the scientific format
+        std :: fixed        shows data in the fixed format without the scitific sign */  
+    // std :: showpoint     show trailing zeros if necessary
+
+
+
+
+    system("pause");
+    return 0;
+}
