068d5f76dad9aa252905bb3156c1c3d45860ed43
Arun Jyoti Mondal <ajmonedu@gmail.com>
2022-09-25 16:20:27 +0600

    done with character

--- CPP/Character/main.cpp
+++ CPP/Character/main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
 
 int main (){
+    char value = 65; 
+    std :: cout << "value : " << value << std:: endl;
+    std :: cout << "value : " << static_cast<int>(value) << std :: endl;
     
+    char character1{'a'};
+    char character2{'r'};
+    char character3{'r'};
+    char character4{'o'};
+    char character5{'w'};
+    
+    std :: cout << character1 << std :: endl;
+    std :: cout << character2 << std :: endl;
+    std :: cout << character3 << std :: endl;
+    std :: cout << character4 << std :: endl;
+    std :: cout << character5 << std :: endl;
+
+    system("pause");
+    return 0;
 }
