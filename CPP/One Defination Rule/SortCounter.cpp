d76fcfdca61601792d51c03e0b34763e400e808a
Arun Jyoti Mondal <ajmonedu@gmail.com>
2022-10-02 23:02:17 +0600

    tested selection sorting algo in cpp

---
+++ CPP/One Defination Rule/SortCounter.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+int main(){
+    
+    int array[] {12,34,45,62,2,5,3,6,1};
+    
+
+    int size = (sizeof(array)/sizeof(array[0]));
+
+    for(int i =0; i<size-1 ; i++){
+        for(int j = i+1; j<size; j++){
+            if(array[i] > array[j]){
+                int temp = array[j];
+                array[j] = array [i];
+                array [i] = temp;
+            }
+            
+
+        }
+    }
+
+    for(int i = 0; i<size-1; i++){
+        std :: cout <<  " "  << array[i] << std :: endl;
+    }
+
+ 
+
+    system("pause");
+    return 0;
+}
+
