ac490df0ee9d683357c34cbea6e5faa63bf39bcd
Arun Jyoti Mondal <ajmonedu@gmail.com>
2022-10-06 23:32:11 +0600

    changed

--- CPP/One Defination Rule/main2.cpp
+++ CPP/One Defination Rule/main2.cpp
@@ -6,6 +6,7 @@ int main(){
     else if (__cplusplus == 201103L) std::cout << "C++11\n";
     else if (__cplusplus == 199711L) std::cout << "C++98\n";
     else std::cout << "pre-standard C++\n";
+    std :: cout << "This line should be printed." << std :: endl;
 
 
     system("pause");
