8f6f42c6c3f2c93e8ce8a7291be17788821024cc
Arun Jyoti Mondal <ajmonedu@gmail.com>
2022-10-15 23:56:45 +0600

    committed

--- CPP/draft.cpp
+++ CPP/draft.cpp
@@ -6,6 +6,7 @@ int main(){
     else if (__cplusplus == 201103L) std::cout << "C++11\n";
     else if (__cplusplus == 199711L) std::cout << "C++98\n";
     else std::cout << "pre-standard C++\n";
+    else std::cout << "pre-standard C++\n";
 
 
     system("pause");
